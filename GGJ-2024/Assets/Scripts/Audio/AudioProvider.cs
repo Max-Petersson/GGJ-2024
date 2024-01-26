d68b435becf9ce633180257bfead3c50c55d2116
Risalord <max.hej777@gmail.com>
2024-01-26 21:58:30 +0100

    Added some sounds

---
+++ GGJ-2024/Assets/Scripts/Audio/AudioProvider.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public interface IAudioService
+{
+    public void PlayAudioClip(string clipName);
+    public void StopAudioClip(string clipName);
+}
+public class AudioProvider : MonoBehaviour
+{
+    private static IAudioService m_audioService;
+    public static IAudioService AudioService 
+    {
+        get { return m_audioService; }
+        set { m_audioService ??= new NullAudioService(); }
+    }
+    
+    public static void SetService(IAudioService service)
+    {
+        if(service == null)
+        {
+            AudioService = new NullAudioService();
+            return;
+        }
+
+        m_audioService = service;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+public class AudioService : IAudioService
+{
+    public void PlayAudioClip(string clipName)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void StopAudioClip(string clipName)
+    {
+        throw new System.NotImplementedException();
+    }
+}
+public class NullAudioService : IAudioService
+{
+    public void PlayAudioClip(string clipName)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void StopAudioClip(string clipName)
+    {
+        throw new System.NotImplementedException();
+    }
+}
