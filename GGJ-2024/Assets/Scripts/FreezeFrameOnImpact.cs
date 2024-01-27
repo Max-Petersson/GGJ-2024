92e156fc128ee3a796a8c62563a8153059436198
Thomas Enghult <thomas.enghult@gmail.com>
2024-01-27 22:29:04 +0100

    Add freeze Frame On Impact

--- GGJ-2024/Assets/Scripts/FreezeFrameOnImpact.cs
+++ GGJ-2024/Assets/Scripts/FreezeFrameOnImpact.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class FreezeFrameOnImpact : MonoBehaviour
 {
-    [SerializeField] private float velocityForImpactFrame;
+    [SerializeField] private float velocityForImpactFrame = 25f;
     [SerializeField] private bool freezeTimescale;
-    [SerializeField] private float freezeTime;
-    [SerializeField] private float cooldown;
+    [SerializeField] private float freezeTime = 0.2f;
+    [SerializeField] private float cooldown = 5f;
+
+    [SerializeField] private GameObject particleEffect;
     private Rigidbody2D rb;
     private float timer;
 
@@ -29,21 +31,30 @@ public class FreezeFrameOnImpact : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+
+        if (collision.rigidbody == rb)
+            return;
+
         if (rb.velocity.magnitude < velocityForImpactFrame)
             return;
 
         if (Time.time < timer + cooldown)
             return;
+        var contact = collision.GetContact(0);
+        Vector2 moveToPoint = contact.point + contact.normal * 0.5f;
 
+        transform.position = moveToPoint;
         accumulatedForce += collision.relativeVelocity;
         TriggerFreeze();
-        
+        Debug.Log("Collided with " + collision.transform.name, collision.transform);
     }
 
     private void TriggerFreeze()
     {
         timer = Time.time;
         AudioSource.PlayClipAtPoint(hitSound, transform.position);
+        GameObject effect = Instantiate(particleEffect, transform.position, transform.rotation);
+        Destroy(effect, freezeTime);
         StartCoroutine(CoFreezeFrame(freezeTime));
     }
 
